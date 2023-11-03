2bb99fb00eedd0a88b6a5014e9e22a8ba338b033
Noah Milberger <n.milberger@gmail.com>
2023-11-03 16:11:47 -0400

    Updated default metadata in layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,37 +6,21 @@ import { PropsWithChildren } from 'react';
 import '@/styles/main.css';
 
 const meta = {
-  title: 'Next.js Subscription Starter',
-  description: 'Brought to you by Vercel, Stripe, and Supabase.',
-  cardImage: '/og.png',
+  title: 'Babel Fish AI',
+  description: 'Brought to you by Sync Labs',
   robots: 'follow, index',
   favicon: '/favicon.ico',
-  url: 'https://subscription-starter.vercel.app',
   type: 'website'
 };
 
 export const metadata = {
   title: meta.title,
   description: meta.description,
-  cardImage: meta.cardImage,
   robots: meta.robots,
-  favicon: meta.favicon,
-  url: meta.url,
-  type: meta.type,
-  openGraph: {
-    url: meta.url,
-    title: meta.title,
-    description: meta.description,
-    cardImage: meta.cardImage,
-    type: meta.type,
-    site_name: meta.title
-  },
   twitter: {
     card: 'summary_large_image',
-    site: '@vercel',
     title: meta.title,
-    description: meta.description,
-    cardImage: meta.cardImage
+    description: meta.description
   }
 };
 
