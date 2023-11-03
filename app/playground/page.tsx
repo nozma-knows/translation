c7bb6f5b699b72bf42d8ed58aaee7f76ab2c62eb
Noah Milberger <n.milberger@gmail.com>
2023-11-03 16:12:37 -0400

    Added Title and MediaInput to playground page.

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,9 +1,17 @@
 import { getSession } from '../supabase-server';
+import MediaInput from '@/components/ui/MediaInput';
 import { Flex, Stack, Text } from '@chakra-ui/react';
 import { redirect } from 'next/navigation';
 
+interface Language {
+  name: string;
+  code: string;
+}
+
 export default async function Playground() {
   const session = await getSession();
+
+  const title = `Translate any video to any language, with perfectly matched lip movements`;
 
   if (!session) {
     return redirect('/signin');
@@ -11,16 +19,13 @@ export default async function Playground() {
 
   return (
     <Flex w="full" px={4} justifyContent={'center'} color="white">
-      <Stack w="full" maxW="4xl" py={8} gap={0}>
-        <Text
-          textAlign={'start'}
-          fontSize={['4xl', '6xl']}
-          fontWeight="extrabold"
-          color="white"
-          className="sm:text-center"
-        >
-          Playground
+      <Stack w="full" maxW="4xl" py={8} gap={8}>
+        <Text alignSelf={'center'} fontWeight="bold" fontSize={'2xl'}>
+          {title}
         </Text>
+        <Flex w="full" justifyContent={'center'}>
+          <MediaInput />
+        </Flex>
       </Stack>
     </Flex>
   );
