88cb35ea6b5047eb1e3beef676162cb28542ee0b
Noah Milberger <n.milberger@gmail.com>
2023-11-05 00:45:34 -0400

    Created StatusTag component.

---
+++ components/ui/StatusTag/StatusTag.tsx
@@ -0,0 +1,44 @@
+import { Database } from '@/types_db';
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { FC } from 'react';
+
+// type Job = Database['public']['Tables']['jobs']['Row'];
+type Status = Database['public']['Enums']['status'];
+
+interface Props {
+  status: Status;
+}
+
+const StatusBox: FC<Props> = ({ status }) => {
+  let color = '';
+  switch (status) {
+    case 'pending':
+      color = 'blue.900';
+      break;
+    case 'processing':
+      color = 'yellow.900';
+      break;
+    case 'completed':
+      color = 'green.600';
+      break;
+    case 'failed':
+      color = 'red.600';
+      break;
+  }
+
+  return (
+    <Flex
+      justifyContent={'flex-start'}
+      alignItems={'center'}
+      gap="2"
+      fontSize={'xs'}
+    >
+      <Box bg={color} w="3" aspectRatio={1 / 1} rounded="full" />
+      <Text w="fit" css={{ textTransform: 'capitalize' }}>
+        {status}
+      </Text>
+    </Flex>
+  );
+};
+
+export default StatusBox;
