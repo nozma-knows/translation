278c47a9c92e121628be731e2df1fb604a046e6c
Noah Milberger <n.milberger@gmail.com>
2023-11-05 00:42:08 -0400

    Added table to usage page.

--- app/usage/page.tsx
+++ app/usage/page.tsx
@@ -1,9 +1,26 @@
-import { getSession } from '../supabase-server';
-import { Flex, Stack, Text } from '@chakra-ui/react';
+import { getSession, getJobs } from '../supabase-server';
+import StatusTag from '@/components/ui/StatusTag';
+import {
+  Flex,
+  Stack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tooltip,
+  Tr
+} from '@chakra-ui/react';
+import { DateTime } from 'luxon';
 import { redirect } from 'next/navigation';
+import { FaCheck } from 'react-icons/fa';
+import { FaX } from 'react-icons/fa6';
 
 export default async function Usage() {
   const session = await getSession();
+  const jobs = await getJobs(session?.user.id as string);
 
   if (!session) {
     return redirect('/signin');
@@ -21,6 +38,40 @@ export default async function Usage() {
         >
           Usage
         </Text>
+        <TableContainer>
+          <Table colorScheme={'whiteAlpha'}>
+            <Thead>
+              <Tr>
+                <Th>Date</Th>
+                <Th>Status</Th>
+                <Th isNumeric>Credits</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {jobs?.map((job) => {
+                const createdAt = DateTime.fromJSDate(
+                  new Date(job.created_at)
+                ).toFormat("MMM. d, yyyy 'at' h:mm a");
+                const createdDate = DateTime.fromJSDate(
+                  new Date(job.created_at)
+                ).toFormat('MMM. d, yyyy');
+                return (
+                  <Tr key={job.id}>
+                    <Td>
+                      <Tooltip hasArrow label={createdAt}>
+                        {createdDate}
+                      </Tooltip>
+                    </Td>
+                    <Td>
+                      <StatusTag status={job.status!} />
+                    </Td>
+                    <Td isNumeric>{job.credits}</Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
       </Stack>
     </Flex>
   );
