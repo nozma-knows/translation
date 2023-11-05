26886887f05a8b832eb09a9ef6320cbe7ed827a9
Noah Milberger <n.milberger@gmail.com>
2023-11-05 14:07:52 -0500

    Small update to signin page to fix warning.

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -29,7 +29,7 @@ export default async function SignIn() {
                 src={logo}
                 alt={'Synchronicity Labs Logo'}
                 fill
-                objectFit="contain"
+                style={{ objectFit: 'contain' }}
               />
             </Box>
           </Link>
